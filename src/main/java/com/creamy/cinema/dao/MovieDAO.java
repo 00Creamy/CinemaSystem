46b9ea60fac93ccc1db23807d3b67e623bcf4809
Creamy <dausazam@gmail.com>
2025-06-28 21:45:08 +0800

    Initial commit

--- src/main/java/com/creamy/cinema/dao/MovieDAO.java
+++ src/main/java/com/creamy/cinema/dao/MovieDAO.java
@@ -12,13 +12,6 @@ public class MovieDAO {
     public static void createMovie(DBConnection connection, Movie movie) throws CinemaException {
         try {
             PreparedStatement statement = connection.prepareInsertStatement("INSERT INTO movie (name, rows, seat_per_row) VALUES (?, ?, ?)");
-
-            statement.setString(1, hall.getHallName());
-            statement.setInt(2, hall.getRows());
-            statement.setInt(3, hall.getSeatPerRow());
-
-            int hallId = connection.executeInsertStatement(statement);
-            hall.setHallId(hallId);
         } catch (SQLException e) {
             throw new CinemaException("Error when trying to create hall.", e);
         }
